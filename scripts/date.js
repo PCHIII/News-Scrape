4107b8d2bde88fe324a0306574a8df35a1e7822e
PCHIII <treyhelmer@gmail.com>
2019-10-07 00:07:57 -0400

    setup date.js

--- scripts/date.js
+++ scripts/date.js
@@ -1,1 +1,13 @@
-var markDate = function() {}
+var markDate = function() {
+    var d = new Date();
+    var formattedDate = "";
+
+    formattedDate += (d.getMonth() + 1 ) + "_";
+    formattedDate += d.getDate() + "_";
+    formattedDate +=d.getFullYear();
+
+    return formattedDate;
+
+};
+
+module.exports = makeDate;
