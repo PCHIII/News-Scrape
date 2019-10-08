7c11d43d216894ce54b90c08cb245ab0fe4f8795
PCHIII <treyhelmer@gmail.com>
2019-10-08 00:20:01 -0400

    axios working

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -3,21 +3,48 @@
 
 var request = require("request");
 var cheerio = require("cheerio");
+var axios = require("axios");
+
+var scrape = function() {
+    return axios.get("https://www.nytimes.com/").then(res => {
+      var $ = cheerio.load(res.data);
+      var articles = [];
+  
+      $(".assetWrapper").each(function(i, element) {
+        var head = $(this)
+          .find("h2")
+          .text()
+          .trim();
+  
+        // var url = $(this)
+        //   .find("a")
+        //   .attr("href");
+  
+        var sum = $(this)
+          .find("p")
+          .text()
+          .trim();
+        
+        // var img = $(this)
+        //   .find("img")
+        //   .attr("src");
 
 
-var scrape = function (cb) {
-    request("http://www.nytimes.com/section/politics", function(err, res, body) {
-
-        var $ = cheerio.load(body);
-
-        var articles = [];
-
-        $("article").each(function(i, element) {
-// grab story heading and summary and remove white space.
-            var head = $(this).children("h2").text().trim();
 
 
-            var sum = $(this).children("p").text().trim();
+// var scrape = function (cb) {
+//     request("http://www.nytimes.com/", function(err, res, body) {
+
+//         var $ = cheerio.load(body);
+
+//         var articles = [];
+
+//         $(".assetWrapper").each(function(i, element) {
+// // grab story heading and summary and remove white space.
+//             var head = $(this).children("h2").text().trim();
+
+
+//             var sum = $(this).children("p").text().trim();
 // replace regexp method line break and cleanup
             if (head && sum) {
                 var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
@@ -27,14 +54,14 @@ var scrape = function (cb) {
                     headLine: headNeat,
                     summary: sumNeat
                 };
-
+console.log(dataToAdd)
                 articles.push(dataToAdd);
             }
         });
 
 // call back function to returning articles
-        cb(articles);
-
+        // cb(articles);
+        return articles;
     });
 };
 // export scrape to the app
