0b4ffd70460442db15e25ffdb39e6fe9f264fafb
PCHIII <treyhelmer@gmail.com>
2019-10-09 11:12:32 -0400

    updated notes modal

--- public/assets/javascript/saved.js
+++ public/assets/javascript/saved.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
      function initPage() {
          articleContainer.empty();
          $.get("/api/headlines?saved=true").then(function(data) {
-
          if (data && data.length) {
              renderArticles(data);
          }
@@ -22,24 +21,19 @@ $(document).ready(function() {
  }
 
     function renderArticles(articles) {
-
         var articlePanels = [];
-
         for (var i = 0; i < articles.length; i++) {
             articlePanels.push(createCard(articles[i]));
-
         }
         articleContainer.append(articlePanels);
 }
     function createCard(article) {
-
         var card = $(
         [
         "<div class = 'card  shadow mb-4'>",
         "<div class='card-header bg-light'>",
         "<h4>",
         article.headline,
-        
         "</h4>",
         "</div>",
         "<div class='card-body bg-white text-dark'>",
@@ -52,7 +46,7 @@ $(document).ready(function() {
         "<div class ='card-text bg-white pb-2 pl-2'>",
         "<a class='btn btn-danger delete mr-2 text-white'>",
         "Delete Article</a>",
-        "<a class='btn btn-info notes text-white'>Add Notes</a>",
+        "<a class='btn btn-info notes text-white'>Article Notes</a>",
         "</div>",
 
         "</div>"
@@ -68,15 +62,15 @@ $(document).ready(function() {
         var emptyAlert = $(
         [
         "<div class='alert alert-warning text-center'>",
-        "<h4>No saved Articles currently available.</h4>",
+        "<h4>No saved Articles Currently Available.</h4>",
         "</div>",
         "<div class='card'>",
         "<div class='card-header text-center text-danger'>",
-        "<h3> Do you want to check available Articles?</h3>",
+        "<h3> Do you want to Browse Available Articles?</h3>",
         "</div>",
         "<div class='card-body text-center'>",
         
-        "<h4><a href='/'>Check Articles</a></h4>",
+        "<h4><a href='/'>Browse Articles</a></h4>",
         "</div>",
         "</div>"
 
@@ -88,19 +82,14 @@ $(document).ready(function() {
     function renderNotesList(data) {
         var notesToRender = [];
         var currentNote;
-
         if (!data.notes.length) {
-
-            currentNote = [
-                "<li class='list-group-item'>",
-                "No notes added for this article currently.",
-                "</li>"
+            currentNote = ["<li class='list-group-item'>",
+                "No notes added for this article currently.", "</li>"
             ].join("");
             notesToRender.push(currentNote);
         }
         else {
             for (var i = 0; i < data.notes.length; i++) {
-
                 currentNote = $(
                     [
                     "<li class='list-group-item note'>",
@@ -123,39 +112,32 @@ function handleArticleDelete() {
     }).then(function(data) {
         if (data.ok) {
             initPage();
-
         }
     });
 }
 
-
     function handleArticleNotes() {
-
         var currentArticle = $(this).parents(".card").data();
-
         $.get("/api/notes/" + currentArticle._id).then(function(data) {
-
             var modalText =  [
                 "<div class='card border-0 pl-3'>",
                 "<h5>Notes for Article: ",
                 currentArticle._id,
                 "</h5>",
                 "<hr />",
-                "<ul class='list-group note-container mb-3'>",
+                "<ul class='list-group  mb-3 note-container'>",
                 "</ul>",
-                "<textarea placeholder=' New Note' rows='10' cols='60''></textarea>",
-                "<button class='btn btn-success save ml-auto mt-2'>Save Note</button>",
+                "<textarea placeholder='New Note' rows='10' cols='60''></textarea>",
+                "<button class='btn btn-success  ml-auto mt-2 save'>Save Note</button>",
                 "</div>"
             ].join("");
             bootbox.dialog({
                 message: modalText,
                 closeButton: true
             });
-
             var noteData = {
-
-            _id: currentArticle._id,
-            notes: data || []
+                _id: currentArticle._id,
+                notes: data || []
             };
 
             $(".btn.save").data("article", noteData);
@@ -167,7 +149,6 @@ function handleArticleDelete() {
     function handleNoteSave() {
         var noteData;
         var newNote = $(".bootbox-body textarea").val().trim();
-
         if (newNote) {
             noteData = {
                 _id: $(this).data("article")._id,
@@ -175,19 +156,16 @@ function handleArticleDelete() {
             };
             $.post("/api/notes", noteData).then(function() {
                 bootbox.hideAll();
-
-                // console.log(newNote)
             });
         }
     }
 
     function handleNoteDelete() {
-
         var noteToDelete = $(this).data("_id");
         $.ajax({
             url: "/api/notes/" + noteToDelete,
             method: "DELETE"
-            
+       
         }).then(function() {
             bootbox.hideAll();
         
